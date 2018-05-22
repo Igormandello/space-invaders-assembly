668200e95ed1c1a2137ee63ceb69011505730539
Igormandello <igormandello@gmail.com>
2018-05-22 08:03:43 -0300

    Changes .gitignore and run.bat

--- run.bat
+++ run.bat
@@ -1,30 +1,8 @@
 @echo off
 
-cd src/bin/
+cd src/
 
 if exist main.obj del main.obj
 if exist main.exe del main.exe
 
-ml /c /coff ../main.asm
-if errorlevel 1 goto errasm
-
-Link /SUBSYSTEM:CONSOLE /OPT:NOREF main.obj
-if errorlevel 1 goto errlink
-
-cls
-
-main.exe
-goto end
-
-:errlink
-echo _
-echo Link error
-goto end
-
-:errasm
-echo _
-echo Assembly Error
-goto end
-
-:end
-cd ../../
+bldall main && main.exe && cls && cd ../
