4cfe1027263d45b9a43c7733e057cc873fe5c030
Igor Mandello <igormandello@gmail.com>
2018-05-26 21:42:56 -0300

    Updates run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,35 @@
 @echo off
 
-cd src/
+if not exist bin mkdir bin
+
+cd bin
+
+rc /v ../src/rsrc.rc
+cvtres /machine:ix86 ../src/rsrc.res
 
 if exist main.obj del main.obj
 if exist main.exe del main.exe
 
-bldall main && main.exe && cls && cd ../
+ml /c /coff ../src/main.asm
+if errorlevel 1 goto errasm
+
+if not exist ../src/rsrc.obj goto nores
+
+Link /SUBSYSTEM:WINDOWS /OPT:NOREF main.obj ../src/rsrc.obj
+if errorlevel 1 goto errlink
+
+main.exe
+goto TheEnd
+
+:errlink
+echo _
+echo Link error
+goto TheEnd
+
+:errasm
+echo _
+echo Assembly Error
+goto TheEnd
+
+:TheEnd
+cd ../
